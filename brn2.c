5e1e6fe10bd780148a63c12acca0d8ac040cd566
lucas-mior <lucas.mior@tutamail.com>
2023-02-28 18:17:04 -0300

    check for calloc error

--- brn2.c
+++ brn2.c
@@ -64,6 +64,16 @@ void *ealloc(void *old, size_t size) {
     }
 }
 
+void *ecalloc(size_t nmemb, size_t size) {
+    void *p;
+    if ((p = calloc(nmemb, size))) {
+        return p;
+    } else {
+        fprintf(stderr, "Failed to allocate memory.\n");
+        exit(1);
+    }
+}
+
 ulong hash(char *str, ulong max) {
     /* djb2 hash function */
     ulong hash = 5381;
@@ -178,7 +188,7 @@ bool insert(SameHash *sh, ulong index, char *newkey) {
         }
         it = it->next;
     } while (it->next);
-    it->next = calloc(1, sizeof (SameHash));
+    it->next = ecalloc(1, sizeof (SameHash));
     it->key = newkey;
 
     return false;
@@ -192,13 +202,14 @@ bool verify(FileList old, FileList new) {
 
     bool rep = false;
     if (new.len > 100) {
-        SameHash *strings = calloc(new.len, sizeof(SameHash));
+        SameHash *strings = ecalloc(new.len, sizeof(SameHash));
         for (size_t i = 0; i < new.len; i += 1) {
             char *name = new.files[i].name;
             ulong h = hash(name, new.len);
             rep = insert(strings, h, name) || rep;
         }
     } else {
+        /* for short lists of filenames, use naive approach */
         for (size_t i = 0; i < new.len; i += 1) {
             char *name = new.files[i].name;
             size_t len = new.files[i].len;
