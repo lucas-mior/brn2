32d2c5a1d39181cffeb6f0981815fdf709d76f2d
lucas-mior <lucas.mior@tutamail.com>
2023-08-25 04:34:50 -0300

    jenkins does not need length

--- hash.h
+++ hash.h
@@ -17,6 +17,6 @@
 
 typedef struct HashTable HashTable;
 
-bool hash_insert(HashTable *, char *, size_t);
+bool hash_insert(HashTable *, char *);
 HashTable *hash_table_create(size_t);
 void hash_table_destroy(HashTable *);
