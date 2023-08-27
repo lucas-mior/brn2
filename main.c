11b6c5d62e01a7664025ed028038f7cb542c5ccf
lucas-mior <lucas.mior@tutamail.com>
2023-08-27 01:44:34 -0300

    benchmark parallel

--- main.c
+++ main.c
@@ -84,9 +84,9 @@ int main(int argc, char **argv) {
         char *args[] = { EDITOR, buffer.name, NULL };
 
         while (true) {
-            util_command(ARRAY_LENGTH(args), args);
-            new = main_file_list_from_lines(buffer.name, old->length);
-            if (!main_verify(old, new)) {
+        /*     util_command(ARRAY_LENGTH(args), args); */
+            /* new = main_file_list_from_lines(buffer.name, old->length); */
+            if (!main_verify(old, old)) {
                 main_free_file_list(new);
                 printf("Fix your renames. Press control-c to cancel or press"
                        " ENTER to open the file list editor again.\n");
@@ -97,6 +97,7 @@ int main(int argc, char **argv) {
             }
         }
     }
+    exit(0);
 
     {
         size_t number_changes = main_get_number_changes(old, new);
