525948fcb6405c756a69029ec92ae1e54e5e902d
lucas-mior <lucas.mior@tutamail.com>
2023-09-20 18:30:00 -0300

    only define _GNU_SOURCE if __linux__

--- brn2.h
+++ brn2.h
@@ -17,7 +17,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifdef __linux__
 #define _GNU_SOURCE
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
