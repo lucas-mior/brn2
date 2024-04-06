a766b8a04341b26ec235221882ee1de4c714dd4b
lucas-mior <lucas.mior@tutamail.com>
2024-04-06 16:55:56 -0300

    setup hash.c test

--- hash.c
+++ hash.c
@@ -15,6 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <assert.h>
 #include <math.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -338,22 +339,27 @@ random_string(void) {
     return random_string;
 }
 
+
+#ifndef MAIN
+#define MAIN 0
+#endif
+
+#if MAIN
+// flags: util.o -lm
 #define NSTRINGS 4096
-
-static void
-hash_test(void **state) {
+int main(int argc, char **argv) {
     HashMap *map = hash_map_create(NSTRINGS);
-    assert_non_null(map);
-    assert_true(hash_map_capacity(map) >= NSTRINGS);
+    assert(map);
+    assert(hash_map_capacity(map) >= NSTRINGS);
 
-    assert_true(hash_map_insert(map, "a", 0));
-    assert_false(hash_map_insert(map, "a", 1));
-    assert_true(hash_map_insert(map, "b", 2));
+    assert(hash_map_insert(map, "a", 0));
+    assert(!hash_map_insert(map, "a", 1));
+    assert(hash_map_insert(map, "b", 2));
 
     for (int i = 0; i < NSTRINGS; i += 1) {
         char *key = random_string();
         int value = rand();
-        assert_true(hash_map_insert(map, key, value));
+        assert(hash_map_insert(map, key, value));
     }
 
     printf("\nOriginal hash map:\n");
@@ -363,51 +369,21 @@ hash_test(void **state) {
     map = hash_map_balance(map);
     printf("\nAfter balance:\n");
     hash_map_print_summary(map);
-    assert_true(collisions_before > hash_map_collisions(map));
+    assert(collisions_before > hash_map_collisions(map));
 
-    assert_true(hash_map_length(map) == (2 + NSTRINGS));
-    assert_true(*(uint32 *) hash_map_lookup(map, "a") == 0);
-    assert_null(hash_map_lookup(map, "c"));
+    assert(hash_map_length(map) == (2 + NSTRINGS));
+    assert(*(uint32 *) hash_map_lookup(map, "a") == 0);
+    assert(!hash_map_lookup(map, "c"));
 
-    assert_false(hash_map_remove(map, "c"));
-    assert_true(hash_map_remove(map, "b"));
+    assert(!hash_map_remove(map, "c"));
+    assert(hash_map_remove(map, "b"));
 
-    assert_true(hash_map_length(map) == (1 + NSTRINGS));
+    assert(hash_map_length(map) == (1 + NSTRINGS));
 
-    assert_true(hash_map_remove(map, "a"));
+    assert(hash_map_remove(map, "a"));
 
     hash_map_free_keys(map);
     hash_map_destroy(map);
-    (void) state;
-    return;
+    exit(0);
 }
-
-static bool
-contains_filename(FileList *list, FileName file) {
-    for (uint32 i = 0; i < list->length; i += 1) {
-        if (!strcmp(list->files[i].name, file.name))
-            return true;
-    }
-    return false;
-}
-
-static void
-brn2_test(void **state) {
-    FileList *list1;
-    FileList *list2;
-    char *command = "ls -a > /tmp/brn2test";
-    char *file = command + 8;
-    system(command);
-    list1 = brn2_list_from_dir(".");
-    list2 = brn2_list_from_lines(file, 0);
-
-    assert_true(list1->length == list2->length);
-
-    for (uint32 i = 0; i < list1->length; i += 1)
-        assert_true(contains_filename(list2, list1->files[i]));
-
-    brn2_free_list(list1);
-    brn2_free_list(list2);
-    unlink(file);
-    (void) state;
-}
+#endif
