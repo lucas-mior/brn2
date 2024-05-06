61894d2565a9977d36a0799b0f8d0f8b2840d81d
lucas-mior <lucas.mior@tutamail.com>
2024-05-06 14:28:50 -0300

    update copyright

--- hash.h
+++ hash.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Mior, Lucas; <lucasbuddemior@gmail.com>
+ * Copyright (C) 2024 Mior, Lucas; <lucasbuddemior@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
