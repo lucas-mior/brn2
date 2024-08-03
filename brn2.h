2b2c2c5fdf37b5021d6aeadeedc362e89abf5215
lucas-mior <lucas.mior@tutamail.com>
2024-08-03 20:24:52 -0300

    limit threads to MAX_THREADS

--- brn2.h
+++ brn2.h
@@ -40,6 +40,7 @@
 #define USE_HASH_MAP_THRESHOLD 128
 #define USE_THREADS_THRESHOLD 524288
 #define MAX_THREADS 64
+#define MIN(a,b) (a) < (b) ? (a) : (b)
 
 #define STRUCT_ARRAY_SIZE(Struct, ArrayType, array_length) \
     sizeof (*Struct) + (usize)(array_length) * sizeof (ArrayType)
