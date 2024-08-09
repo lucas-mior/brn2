397e6972171812acc6b71aed6344b9e484151496
lucas-mior <lucas.mior@tutamail.com>
2024-08-09 08:12:14 -0300

    use hash set when lookup is not necessary

--- hash.h
+++ hash.h
@@ -57,6 +57,7 @@ uint32 hash_map_length(HashMap *);
 uint32 hash_map_collisions(HashMap *);
 uint32 hash_map_expected_collisions(HashMap *);
 
+#define hash_set_create(a)                   hash_map_create(a)
 #define hash_set_insert(a, b)                hash_map_insert(a, b, 0)
 #define hash_set_insert_pre_calc(a, b, c, d) hash_map_insert_pre_calc(a, b, c, d, 0)
 
