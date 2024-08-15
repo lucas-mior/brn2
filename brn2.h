17a59a70e165120026ba77ac442d05b92f4d9de7
lucas-mior <lucas.mior@tutamail.com>
2024-08-15 19:03:59 -0300

    number_changes using threads

--- brn2.h
+++ brn2.h
@@ -107,10 +107,11 @@ FileList *brn2_list_from_args(int, char **);
 void brn2_normalize_names(FileList *);
 Hash *brn2_create_hashes_threads(FileList *, uint32);
 bool brn2_verify(FileList *, FileList *, HashMap *, Hash *);
+int brn2_thread_changes(void *);
 uint32 brn2_get_number_changes(FileList *, FileList *);
-uint32 brn2_execute(FileList *old, FileList *new,
-                    HashMap *oldlist_map,
-                    Hash *hashes_old, Hash *hashes_new, bool quiet);
+uint32 brn2_execute(FileList *, FileList *,
+                    HashMap *,
+                    Hash *, Hash *, bool);
 
 void brn2_free_lines_list(FileList *);
 void brn2_free_dir_list(FileList *);
