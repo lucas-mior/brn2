31b6bbcfec26b238b3e6202eb751bc8b8f25ae0b
lucas-mior <lucas.mior@tutamail.com>
2024-09-03 14:09:32 -0300

    change hash_normal to take HashMap and use correct bitmask even for edge cases

--- brn2.c
+++ brn2.c
@@ -292,7 +292,7 @@ brn2_work_hashes(void *arg) {
         FileList *list = slice->old_list;
         FileName *newfile = &(list->files[i]);
         newfile->hash = hash_function(newfile->name);
-        slice->hashes[i].mod = hash_normal(slice->map_capacity, newfile->hash);
+        slice->hashes[i].mod = newfile->hash % slice->map_capacity;
     }
     thrd_exit(0);
 }
