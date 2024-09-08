c539181c946462e9e42e2f22d947b3c51b1b0696
lucas-mior <lucas.mior@tutamail.com>
2024-09-08 19:14:30 -0300

    fish completions working more or less

--- completions/brn2.fish
+++ completions/brn2.fish
@@ -1,23 +1,18 @@
-# Save this as _clipsim.fish in a directory listed in your $fish_complete_path, such as ~/.config/fish/completions/
-
-complete -c clipsim -l print -d 'print entire history, with trimmed whitespace'
-complete -c clipsim -s p -d 'print entire history, with trimmed whitespace'
-complete -c clipsim -l info -d 'print entry number <n>, with original whitespace' -a '(_clipsim_entries)'
-complete -c clipsim -s i -d 'print entry number <n>, with original whitespace' -a '(_clipsim_entries)'
-complete -c clipsim -l copy -d 'copy entry number <n>, with original whitespace' -a '(_clipsim_entries)'
-complete -c clipsim -s c -d 'copy entry number <n>, with original whitespace' -a '(_clipsim_entries)'
-complete -c clipsim -l remove -d 'remove entry number <n>' -a '(_clipsim_entries)'
-complete -c clipsim -s r -d 'remove entry number <n>' -a '(_clipsim_entries)'
-complete -c clipsim -l save -d 'save history to $XDG_CACHE_HOME/clipsim/history'
-complete -c clipsim -s s -d 'save history to $XDG_CACHE_HOME/clipsim/history'
-complete -c clipsim -l daemon -d 'spawn daemon (clipboard watcher and command listener)'
-complete -c clipsim -s d -d 'spawn daemon (clipboard watcher and command listener)'
-complete -c clipsim -l help -d 'print this help message'
-complete -c clipsim -s h -d 'print this help message'
-
-function _clipsim_entries
-  set -l entries (clipsim -p | pcregrep -o1 '^\\s*([0-9]+)\\s*:.*$')
-  complete -c clipsim -a "($entries)"
-end
-
-_clipsim_entries
+complete -c brn2 -s h -d 'Display help and exit.'
+complete -c brn2 -l help -d 'Display this help message and exit.'
+complete -c brn2 -s q -d 'Quiet mode; suppress output messages.'
+complete -c brn2 -l quiet -d 'Quiet mode; suppress output messages.'
+complete -c brn2 -s v -d 'Verbose mode (default); output messages.'
+complete -c brn2 -l verbose -d 'Verbose mode (default); output messages.'
+complete -c brn2 -s c -d 'Check if original file names exist.'
+complete -c brn2 -l check -d 'Check if original file names exist.'
+complete -c brn2 -s s -d 'Disable sorting of original list.'
+complete -c brn2 -l sort -d 'Disable sorting of original list.'
+complete -c brn2 -s F -d 'Exit on first renaming error.'
+complete -c brn2 -l fatal -d 'Exit on first renaming error.'
+complete -c brn2 -s i -d 'Rename files not given in the list of files to rename.'
+complete -c brn2 -l implict -d 'Rename files not given in the list of files to rename.'
+complete -c brn2 -s e -d 'Only rename files given in the list of files to rename (default).'
+complete -c brn2 -l explict -d 'Only rename files given in the list of files to rename (default).'
+complete -c brn2 -s f -d 'Rename files listed in]:filename:_files'
+complete -c brn2 -l filename -d 'Rename files listed in]:filename:_files'
