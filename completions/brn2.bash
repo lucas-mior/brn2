4c0e98852e99dc0e4d66ebc6228c8004440208f9
lucas-mior <lucas.mior@tutamail.com>
2024-09-08 19:08:41 -0300

    bash completions more or less working

--- completions/brn2.bash
+++ completions/brn2.bash
@@ -1,16 +1,7 @@
-# Save this as _clipsim.bash in a directory listed in your $BASH_COMPLETION_DIR, such as /etc/bash_completion.d/ or your own custom directory.
-
-_clipsim_entries() {
-  local entries
-  entries=$(clipsim -p | grep -oP '^\s*\K([0-9]+)(?=\s*:)' | tr '\n' ' ')
-  COMPREPLY=($(compgen -W "$entries" -- "${COMP_WORDS[COMP_CWORD]}"))
-}
-
-_clipsim_completion() {
-  local cur prev commands
+_brn2_completion() {
+  local cur commands
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD-1]}"
   commands=(
     "-p --print"
     "-i --info"
@@ -21,14 +12,7 @@ _clipsim_completion() {
     "-h --help"
   )
 
-  case "${prev}" in
-    -i|--info|-c|--copy|-r|--remove)
-      _clipsim_entries
-      return
-      ;;
-  esac
-
   COMPREPLY=($(compgen -W "${commands[*]}" -- "${cur}"))
 }
 
-complete -F _clipsim_completion clipsim
+complete -F _brn2_completion brn2
