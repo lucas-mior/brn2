89a845e60f6b225532fe7232e2c07d593fd36013
lucas-mior <lucas.mior@tutamail.com>
2024-09-08 19:03:03 -0300

    zsh completions more or less working

--- completions/brn2.zsh
+++ completions/brn2.zsh
@@ -1,27 +1,22 @@
-#compdef clipsim
+#compdef brn2
 
-local -a commands
-commands=(
-    '-p[print entire history, with trimmed whitespace]'
-    '--print[print entire history, with trimmed whitespace]'
-    '-i[print entry number <n>, with original whitespace]: :_clipsim_entries'
-    '--info[print entry number <n>, with original whitespace]: :_clipsim_entries'
-    '-c[copy entry number <n>, with original whitespace]: :_clipsim_entries'
-    '--copy[copy entry number <n>, with original whitespace]: :_clipsim_entries'
-    '-r[remove entry number <n>]: :_clipsim_entries'
-    '--remove[remove entry number <n>]: :_clipsim_entries'
-    '-s[save history to $XDG_CACHE_HOME/clipsim/history]'
-    '--save[save history to $XDG_CACHE_HOME/clipsim/history]'
-    '-d[spawn daemon (clipboard watcher and command listener)]'
-    '--daemon[spawn daemon (clipboard watcher and command listener)]'
-    '-h[print help information]'
-    '--help[print help information]'
-)
-
-_clipsim_entries() {
-    local -a entries
-    entries=($(clipsim -p | sed -zEn '/^[0-9]+ /{s/^([0-9]+).*/\1\n/g;p}'))
-    _describe 'entries' entries
-}
-
-_arguments -s -S $commands
+_arguments \
+  '-h[Display this help message and exit.]'\
+  '--help[Display this help message and exit.]'\
+  '-q[Quiet mode; suppress output messages.]'\
+  '--quiet[Quiet mode; suppress output messages.]'\
+  '-v[Verbose mode (default); output messages.]'\
+  '--verbose[Verbose mode (default); output messages.]'\
+  '-c[Check if original file names exist.]'\
+  '--check[Check if original file names exist.]'\
+  '-s[Disable sorting of original list.]'\
+  '--sort[Disable sorting of original list.]'\
+  '-F[Exit on first renaming error.]'\
+  '--fatal[Exit on first renaming error.]'\
+  '-i[Rename files not given in the list of files to rename.]'\
+  '--implict[Rename files not given in the list of files to rename.]'\
+  '-e[Only rename files given in the list of files to rename (default).]'\
+  '--explict[Only rename files given in the list of files to rename (default).]'\
+  '-f[Rename files listed in]:filename:_files'\
+  '--filename[Rename files listed in]:filename:_files'\
+  '*:remaining argument:_files'
