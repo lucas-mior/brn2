377c6b2d1222d0292c09de9206c7d926e530c081
lucas-mior <lucas.mior@tutamail.com>
2025-03-15 18:48:28 -0300

    fix bug

--- main.c
+++ main.c
@@ -286,7 +286,7 @@ int main(int argc, char **argv) {
             newlist_map = hash_map_create(new->length);
             main_capacity = hash_map_capacity(newlist_map);
             hashes_new = brn2_create_hashes(new, main_capacity);
-            brn2_verify(old, new, newlist_map, hashes_new);
+            brn2_verify(new, newlist_map, hashes_new);
             break;
 #else
             util_command(ARRAY_LENGTH(args_edit), args_edit);
