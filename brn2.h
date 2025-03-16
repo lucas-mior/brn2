37ca81dd6e438b957c9b489b57853865a513b724
lucas-mior <lucas.mior@tutamail.com>
2025-03-15 21:06:57 -0300

    newname_exists in parallel (testing)

--- brn2.h
+++ brn2.h
@@ -134,7 +134,8 @@ void brn2_normalize_names(FileList *, FileList *);
 uint32 *brn2_create_hashes(FileList *, uint32);
 bool brn2_verify(FileList *, HashMap *, uint32 *);
 uint32 brn2_get_number_changes(FileList *, FileList *);
-uint32 brn2_execute(FileList *, FileList *, HashMap *, uint32 *, uint32 *);
+bool *brn2_newname_exists(FileList *);
+uint32 brn2_execute(FileList *, FileList *, HashMap *, uint32 *, uint32 *, bool *);
 void brn2_slash_add(FileName *file);
 void brn2_free_list(FileList *list);
 
