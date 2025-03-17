b11ecebdf96eadd95eec8a22d3e98306fb633493
lucas-mior <lucas.mior@tutamail.com>
2025-03-17 20:17:17 -0300

    delete djb2 and test fnv-1a with 64 bit

--- hash.c
+++ hash.c
@@ -161,16 +161,17 @@ hash_map_destroy(HashMap *map) {
 uint32
 hash_function(char *key, uint32 key_size) {
     BRN2_ASSUME_ALIGNED(key);
-#if 1
+#if 0
     uint32 hash = 2166136261u;
     for (uint32 i = 0; i < key_size; i += 1) {
         hash ^= key[i];
         hash *= 16777619u;
     }
 #else
-    uint32 hash = 5381;
+    uint64 hash = 14695981039346656037u;
     for (uint32 i = 0; i < key_size; i += 1) {
-        hash = ((hash << 5) + hash) + (uint32)key[i];
+        hash ^= key[i];
+        hash *= 1099511628211u;
     }
 #endif
     return (uint32)hash;
