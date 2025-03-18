6d26e1d82046cdf4c5a21118c24fea71d001fded
lucas-mior <lucas.mior@tutamail.com>
2025-03-17 21:19:48 -0300

    clean up

--- arena.h
+++ arena.h
@@ -11,7 +11,6 @@ Arena *arena_alloc(size_t);
 void *arena_push(Arena *arena, uint32);
 void *arena_reset(Arena *arena);
 void *arena_reset_zero(Arena *arena);
-void arena_release(Arena *arena);
 void arena_destroy(Arena *arena);
 
 #endif
