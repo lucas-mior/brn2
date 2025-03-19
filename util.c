b675ab2135b628e67d24c1ca3bdcc4fe825a5d0e
lucas-mior <lucas.mior@tutamail.com>
2025-03-19 16:14:04 -0300

    fix warning

--- util.c
+++ util.c
@@ -186,6 +186,7 @@ int main(void) {
     void *p2 = xcalloc(1024, 1024*1024);
     char *string = __FILE__;
     memcpy(p1, string, strlen(string));
+    memset(p2, 0, 1024*1024*1024);
     char *p3 = xstrdup(p1);
     error("%s == %s is working? %b\n", string, p3, !strcmp(string, p3));
     exit(0);
