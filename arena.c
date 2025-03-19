334aa8c2a4012983f4786edaa76f629adc9569fc
lucas-mior <lucas.mior@tutamail.com>
2025-03-19 11:37:50 -0300

    delete email

--- arena.c
+++ arena.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Mior, Lucas; <lucasbuddemior@gmail.com>
+ * Copyright (C) 2024 Mior, Lucas; 
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
