0330b2544523166ba87f40f0afbd64e138f24917
lucas-mior <lucas.mior@tutamail.com>
2025-04-03 20:18:29 -0300

    style

--- brn2.h
+++ brn2.h
@@ -156,8 +156,8 @@ uint32 *brn2_create_hashes(FileList *, uint32 *, uint32);
 bool brn2_verify(FileList *, HashMap *, uint32 *);
 uint32 brn2_get_number_changes(FileList *, FileList *);
 uint32 brn2_execute(FileList *, FileList *, HashMap *, uint32 *, uint32 *);
-void brn2_slash_add(FileName *file);
-void brn2_free_list(FileList *list);
+void brn2_slash_add(FileName *);
+void brn2_free_list(FileList *);
 
 void brn2_usage(FILE *) __attribute__((noreturn));
 void error(char *, ...);
