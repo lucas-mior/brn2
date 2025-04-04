062706bcfd3dd7e5580f8081474242af3495bdbf
lucas-mior <lucas.mior@tutamail.com>
2025-04-03 23:27:10 -0300

    wip mergesort

--- main.c
+++ main.c
@@ -65,20 +65,34 @@ delete_brn2_buffer(void) {
     return;
 }
 
+static void
+shuffle(void *array, size_t n, size_t size) {
+    char tmp[size];
+    char *arr = array;
+    size_t stride = size * sizeof(char);
+
+    if (n > 1) {
+        size_t i;
+        for (i = 0; i < n - 1; ++i) {
+            size_t rnd = (size_t) rand();
+            size_t j = i + rnd / (RAND_MAX / (n - i) + 1);
+
+            memcpy(tmp, arr + j * stride, size);
+            memcpy(arr + j * stride, arr + i * stride, size);
+            memcpy(arr + i * stride, tmp, size);
+        }
+    }
+}
+
 void
 mergesort(void *base, size_t nitems, size_t size,
           int (*compar)(const void *, const void *)) {
-    if (nitems < 2) return;  // Base case: already sorted
+    if (nitems < 2)
+        return;
 
     size_t mid = nitems / 2;
-    void *left = malloc(mid * size);
-    void *right = malloc((nitems - mid) * size);
-
-    if (!left || !right) {
-        free(left);
-        free(right);
-        return;  // Memory allocation failure
-    }
+    void *left = xmalloc(mid * size);
+    void *right = xmalloc((nitems - mid) * size);
 
     memcpy(left, base, mid * size);
     memcpy(right, (char *)base + mid * size, (nitems - mid) * size);
@@ -226,11 +240,16 @@ int main(int argc, char **argv) {
     struct timespec t1;
     clock_gettime(CLOCK_MONOTONIC_RAW, &t0);
 
-    if (brn2_options_sort) {
-        if (!brn2_options_quiet)
-            printf("Sorting filenames...\n");
-        brn2_threads(brn2_threads_work_sort, old, NULL, NULL, NULL, 0);
-        mergesort(old->files, old->length, sizeof(*(old->files)), brn2_compare);
+    /* if (brn2_options_sort) { */
+    /*     if (!brn2_options_quiet) */
+    /*         printf("Sorting filenames...\n"); */
+    /*     brn2_threads(brn2_threads_work_sort, old, NULL, NULL, NULL, 0); */
+    /* } */
+    shuffle(old->files, old->length, sizeof(*(old->files)));
+    mergesort(old->files, old->length, sizeof(*(old->files)), brn2_compare);
+    /* qsort(old->files, old->length, sizeof(*(old->files)), brn2_compare); */
+    for (uint32 i = 0; i < old->length; i += 1) {
+        printf("[%u] = %s\n", i, old->files[i].name);
     }
 
     clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
