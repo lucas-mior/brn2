8c1c98af3181318cfb090d769a97fd518fad582e
lucas-mior <lucas.mior@tutamail.com>
2025-04-04 11:42:09 -0300

    wip merge_sorted_subarrays

--- sort.c
+++ sort.c
@@ -32,11 +32,77 @@ shuffle(void *array, size_t n, size_t size) {
     }
 }
 
-/* void */
-/* mergesort(void *base, size_t nitems, size_t size, */
-/*           int (*compar)(const void *, const void *)) { */
-/*     return; */
-/* } */
+typedef struct {
+    void *value;
+    int array_index;
+    int element_index;
+} HeapNode;
+
+static void
+swap(HeapNode *a, HeapNode *b) {
+    HeapNode temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+static void
+heapify(HeapNode *heap, int n, int i,
+        int (*compare)(const void *a, const void *b)) {
+    int smallest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if (left < n && compare(heap[left].value, heap[smallest].value) < 0)
+        smallest = left;
+    if (right < n && compare(heap[right].value, heap[smallest].value) < 0)
+        smallest = right;
+    if (smallest != i) {
+        swap(&heap[i], &heap[smallest]);
+        heapify(heap, n, smallest, compare);
+    }
+}
+
+static void
+merge_sorted_subarrays(void *array, int n, int p, usize size, void *dummy,
+                       int (*compare)(const void *a, const void *b)) {
+    char *output = xmalloc(size*n);
+    HeapNode heap[p];
+    char *array2 = array;
+    int nsub = n / p;
+
+    int indices[p];
+    memset(indices, 0, p*sizeof(*indices));
+
+
+    for (int i = 0; i < p; i++) {
+        heap[i].value = xmalloc(size);
+        memcpy(heap[i].value, &array2[i*nsub*size], size);
+        heap[i].array_index = i;
+        heap[i].element_index = 0;
+    }
+
+    for (int i = p / 2 - 1; i >= 0; i--) {
+        heapify(heap, p, i, compare);
+    }
+
+    for (int i = 0; i < n; i++) {
+        memcpy(&output[i*size], heap[0].value, size);
+        int arr_idx = heap[0].array_index;
+        int elem_idx = ++indices[arr_idx];
+
+        if (elem_idx < nsub) {
+            memcpy(heap[0].value, &array2[(arr_idx*nsub + elem_idx)*size], size);
+            heap[0].element_index = elem_idx;
+        } else {
+            memcpy(heap[0].value, dummy, size);
+        }
+
+        heapify(heap, p, 0, compare);
+    }
+
+    memcpy(array2, output, n*size);
+    return;
+}
 
 void
 sort(FileList *old) {
@@ -49,12 +115,23 @@ sort(FileList *old) {
     shuffle(old->files, old->length, sizeof(*(old->files)));
     memcpy(copy, old, lsize);
 
-    brn2_threads(brn2_threads_work_sort, old, NULL, NULL, NULL, 0);
+    int nthreads;
+    nthreads = brn2_threads(brn2_threads_work_sort, old, NULL, NULL, NULL, 0);
     qsort(old->files, old->length, sizeof(*(old->files)), brn2_compare);
 
     clock_gettime(CLOCK_MONOTONIC_RAW, &t0);
+    FileName dummy = {
+        .name = "\077\077",
+        .hash = 0,
+        .length = 0,
+        .type = 0,
+        .unused = 0,
+    };
 
-    /* mergesort(copy->files, copy->length, sizeof(*(copy->files)), brn2_compare); */
+    merge_sorted_subarrays(old->files, old->length, nthreads,
+                           sizeof(*(old->files)),
+                           &dummy, brn2_compare);
+
     qsort(copy->files, copy->length, sizeof(*(copy->files)), brn2_compare);
 
     if (memcmp(copy, old, lsize)) {
@@ -62,9 +139,9 @@ sort(FileList *old) {
     } else {
         error("copy is EQUAL TO old!\n");
     }
-    /* for (uint32 i = 0; i < old->length; i += 1) { */
-    /*     printf("[%u] = %s\n", i, old->files[i].name); */
-    /* } */
+    for (uint32 i = 0; i < old->length; i += 1) {
+        printf("[%u] = %s != %s\n", i, old->files[i].name, copy->files[i].name);
+    }
 
     clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
 
@@ -81,76 +158,6 @@ sort(FileList *old) {
     }
 }
 
-typedef struct {
-    void *value;
-    int array_index;
-    int element_index;
-} HeapNode;
-
-void swap(HeapNode *a, HeapNode *b) {
-    HeapNode temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void heapify(HeapNode *heap, int n, int i,
-             int (*compare)(const void *a, const void *b)) {
-    int smallest = i;
-    int left = 2*i + 1;
-    int right = 2*i + 2;
-
-    if (left < n && compare(heap[left].value, heap[smallest].value) < 0)
-        smallest = left;
-    if (right < n && compare(heap[right].value, heap[smallest].value) < 0)
-        smallest = right;
-    if (smallest != i) {
-        swap(&heap[i], &heap[smallest]);
-        heapify(heap, n, smallest, compare);
-    }
-}
-
-int compare(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b);
-}
-
-void merge_sorted_subarrays(void *array, int n, int nsub, usize size, void *dummy,
-                            int (*compare)(const void *a, const void *b)) {
-    char *output = xmalloc(size*n);
-    HeapNode heap[4];
-    char *array2 = array;
-
-    int indices[4] = {0, 0, 0, 0};
-
-    for (int i = 0; i < 4; i++) {
-        heap[i].value = xmalloc(size);
-        memcpy(heap[i].value, &array2[i*nsub*size], size);
-        heap[i].array_index = i;
-        heap[i].element_index = 0;
-    }
-
-    for (int i = 4 / 2 - 1; i >= 0; i--) {
-        heapify(heap, 4, i, compare);
-    }
-
-    for (int i = 0; i < n; i++) {
-        memcpy(&output[i*size], heap[0].value, size);
-        int arr_idx = heap[0].array_index;
-        int elem_idx = ++indices[arr_idx];
-
-        if (elem_idx < nsub) {
-            memcpy(heap[0].value, &array2[(arr_idx*nsub + elem_idx)*size], size);
-            heap[0].element_index = elem_idx;
-        } else {
-            memcpy(heap[0].value, dummy, size);
-        }
-
-        heapify(heap, 4, 0, compare);
-    }
-
-    memcpy(array2, output, n*size);
-    return;
-}
-
 #ifndef TESTING_THIS_FILE
 #define TESTING_THIS_FILE 0
 #endif
@@ -159,6 +166,11 @@ void merge_sorted_subarrays(void *array, int n, int nsub, usize size, void *dumm
 
 #define SIZE 20
 #define SUB_SIZE (SIZE / 4)
+
+int
+compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
 
 int main(void) {
     int array[SIZE];
@@ -173,7 +185,7 @@ int main(void) {
 
     int dummy = INT_MAX;
 
-    merge_sorted_subarrays(array, SIZE, SUB_SIZE, sizeof(int), &dummy, compare);
+    merge_sorted_subarrays(array, SIZE, 4, sizeof(int), &dummy, compare);
 
     for (int i = 0; i < SIZE; i++) {
         printf("%d ", array[i]);
