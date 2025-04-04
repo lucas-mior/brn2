410f53ad67edae8e2696de919ed32ca39473cc76
lucas-mior <lucas.mior@tutamail.com>
2025-04-04 12:29:26 -0300

    style

--- sort.c
+++ sort.c
@@ -17,7 +17,7 @@ static void
 shuffle(void *array, size_t n, size_t size) {
     char tmp[size];
     char *arr = array;
-    size_t stride = size * sizeof(char);
+    size_t stride = size*sizeof(char);
 
     if (n > 1) {
         size_t i;
@@ -25,9 +25,9 @@ shuffle(void *array, size_t n, size_t size) {
             size_t rnd = (size_t) rand();
             size_t j = i + rnd / (RAND_MAX / (n - i) + 1);
 
-            memcpy(tmp, arr + j * stride, size);
-            memcpy(arr + j * stride, arr + i * stride, size);
-            memcpy(arr + i * stride, tmp, size);
+            memcpy(tmp, arr + j*stride, size);
+            memcpy(arr + j*stride, arr + i*stride, size);
+            memcpy(arr + i*stride, tmp, size);
         }
     }
 }
