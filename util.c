7101f820bb26afccfbd964e4b628bcbd2a0e126c
lucas-mior <lucas.mior@tutamail.com>
2025-04-06 17:54:15 -0300

    style

--- util.c
+++ util.c
@@ -275,7 +275,7 @@ void util_command(const int argc, char **argv) {
 
     if (!success) {
         error("Error running '%s", argv[0]);
-        for (int i = 1; i < argc; i += 1)
+        for (int i = 1; i < (argc - 1); i += 1)
             error(" %s", argv[i]);
         error("': %lu.\n", GetLastError());
         free(cmdline);
