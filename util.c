ddd1b01a82014e09599948fcdc14777ace1a5f9e
lucas-mior <lucas.mior@tutamail.com>
2025-04-07 03:59:57 -0300

    better error messages

--- util.c
+++ util.c
@@ -203,8 +203,8 @@ xstrdup(char *string) {
 
     length = strlen(string) + 1;
     if ((p = malloc(length)) == NULL) {
-        error("Error allocating %zi bytes to duplicate \"%s\".\n",
-               length, string);
+        error("Error allocating %zu bytes to duplicate '%s': %s\n",
+              length, string, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
