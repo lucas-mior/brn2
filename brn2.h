dd36012acffde3e52be2da77628a2e9c4d6ec1e8
lucas-mior <lucas.mior@tutamail.com>
2025-04-08 12:38:44 -0300

    use unsigned integer for BRN2_PATH_MAX

--- brn2.h
+++ brn2.h
@@ -50,7 +50,7 @@
 #define GREEN "\x1b[32m"
 #define RESET "\x1b[0m"
 
-#define BRN2_PATH_MAX 4096
+#define BRN2_PATH_MAX 4096u
 #define BRN2_MIN_PARALLEL 64
 #define BRN2_ALIGNMENT 16
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
