8c9f23f679013c021cdc5235037b89e5fa9605a8
lucas-mior <lucas.mior@tutamail.com>
2025-10-13 10:30:59 -0300

    Revert "names"
    This reverts commit 15a24033e45d8f2fa63e61a1793472d82a16a1fc.

--- brn2.c
+++ brn2.c
@@ -321,10 +321,9 @@ brn2_list_from_lines(FileList *list, char *filename, bool is_old) {
     FILE *lines;
     
     if (!strcmp(filename, "-") || !strcmp(filename, "/dev/stdin")) {
-        int stdin_dup;
         error("Reading from stdin...\n");
-        stdin_dup = dup(STDIN_FILENO);
-        lines = fdopen(stdin_dup, "r");
+        int fd = dup(STDIN_FILENO);
+        lines = fdopen(fd, "r");
     } else {
         if ((lines = fopen(filename, "r")) == NULL) {
             error("Error opening '%s': %s.\n", filename, strerror(errno));
