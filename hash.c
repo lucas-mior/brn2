c1c9eb849605cb4d7bff3ef41e9343ae5adabb8d
lucas-mior <lucas.mior@tutamail.com>
2025-10-29 14:29:46 -0300

    fix integers typedefs

--- hash.c
+++ hash.c
@@ -67,6 +67,12 @@ typedef struct Hash_set HashSet;
 
 #if !defined(INTEGERS)
 #define INTEGERS
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+typedef unsigned long long ulonglong;
+
 typedef int8_t int8;
 typedef int16_t int16;
 typedef int32_t int32;
