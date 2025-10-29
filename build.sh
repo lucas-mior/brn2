33624c08c450d999d83f4b1da66cd80ce98fcded
lucas-mior <lucas.mior@tutamail.com>
2025-10-29 16:07:58 -0300

    check for zig

--- build.sh
+++ build.sh
@@ -132,7 +132,7 @@ case "$target" in
 
         flags="$(awk '/\/\/ flags:/ { $1=$2=""; print $0 }' "$src")"
         if [ $src = "windows_functions.c" ]; then
-            if ! zig --version; then
+            if ! zig version; then
                 continue
             fi
             cmdline="zig cc $CPPFLAGS $CFLAGS"
