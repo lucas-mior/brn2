260a4dfe662a88a45f02670de048cefb15ee4c3c
lucas-mior <lucas.mior@tutamail.com>
2025-10-30 17:47:25 -0300

    use smarter check cleanup

--- main.c
+++ main.c
@@ -196,16 +196,34 @@ main(int argc, char **argv) {
 
     brn2_normalize_names(old, NULL);
 
-    for (uint32 i = 0; i < old->length; i += 1) {
-        FileName **file = &(old->files[i]);
-        while ((*file)->type == TYPE_ERR) {
-            error("Removing '%s' from list.\n", (*file)->name);
-            old->length -= 1;
-            if (old->length <= i) {
-                break;
-            }
-            memmove(file, file + 1, (old->length - i)*sizeof(*file));
+    for (uint32_t i = 0; i < old->length;) {
+        uint32 start;
+        uint32 end;
+        uint32 count;
+
+        if (old->files[i]->type != TYPE_ERR) {
+            i += 1;
+            continue;
         }
+
+        start = i;
+        end = i + 1;
+
+        while (end < old->length && old->files[end]->type == TYPE_ERR) {
+            end += 1;
+        }
+
+        count = end - start;
+        for (uint32 j = start; j < end; j += 1) {
+            error("Removing '%s' from list.\n", old->files[j]->name);
+        }
+
+        if (end < old->length) {
+            memmove(&old->files[start], &old->files[end],
+                    (old->length - end)*sizeof(old->files[0]));
+        }
+
+        old->length -= count;
     }
 
     if (old->length == 0) {
