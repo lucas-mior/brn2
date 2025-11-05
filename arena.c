53f8c3df600cba6b55c4539e629276910529ea25
lucas-mior <lucas.mior@tutamail.com>
2025-11-05 12:11:51 -0300

    more testing

--- arena.c
+++ arena.c
@@ -400,11 +400,27 @@ main(void) {
     int aux;
     assert(arena_pop(arena, &aux) < 0);
 
-    assert(arena_push(arena, SIZEKB(1023)));
+    {
+        void *p1;
+        void *p2;
+
+        assert(p1 = arena_push(arena, SIZEKB(1023)));
+        assert(arena->npushed == 1);
+        assert(p2 = arena_push(arena, SIZEKB(1023)));
+        assert(arena->npushed == 1);
+        assert(arena->next != NULL);
+        assert(arena_of(arena, p1) != arena_of(arena, p2));
+    }
+
+    arena_reset(arena);
+
+    void *p3;
+    void *p4;
+    assert(p3 = arena_push(arena, SIZEKB(500)));
     assert(arena->npushed == 1);
-    assert(arena_push(arena, SIZEKB(1023)));
-    assert(arena->npushed == 1);
-    assert(arena->next != NULL);
+    assert(p4 = arena_push(arena, SIZEKB(500)));
+    assert(arena->npushed == 2);
+    assert(arena_of(arena, p3) == arena_of(arena, p4));
 
     arena_reset(arena);
     assert(arena->pos == arena->begin);
