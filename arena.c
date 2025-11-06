5a10146ecd494f3a3ee299bc7139c8b18c6d9404
lucas-mior <lucas.mior@tutamail.com>
2025-11-06 15:41:19 -0300

    use int64

--- arena.c
+++ arena.c
@@ -478,11 +478,11 @@ main(void) {
     {
         uint32 index = arena_push_index32(arena, 32);
         assert(index != UINT32_MAX);
-        assert((char *)arena->begin + index == (char *)arena->begin);
+        assert(arena->begin + index == (char *)arena->begin);
 
         index = arena_push_index32(arena, 32);
         assert(index != UINT32_MAX);
-        assert((char *)arena->begin + index == (char *)arena->begin + 32);
+        assert(arena->begin + index == (char *)arena->begin + 32);
     }
 
     arena_destroy(arena);
