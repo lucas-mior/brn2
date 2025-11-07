3d7ca8c80d9f0fc8138371c46f5b37d7989009c4
lucas-mior <lucas.mior@tutamail.com>
2025-11-07 15:49:30 -0300

    some util fixes

--- rapidhash.h
+++ rapidhash.h
@@ -61,9 +61,6 @@ typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
-
-typedef size_t usize;
-typedef ssize_t isize;
 #endif
 
 #if defined(__cplusplus)
