b93160dc4bbd6d21d9647a19a106dafe443502e5
lucas-mior <lucas.mior@tutamail.com>
2025-11-14 09:14:55 -0300

    brn2.c: full renaming test

--- hash.c
+++ hash.c
@@ -72,7 +72,7 @@ typedef unsigned char uchar;
 typedef unsigned short ushort;
 typedef unsigned int uint;
 typedef unsigned long ulong;
-typedef unsigned long long ulonglong;
+typedef unsigned long long ullong;
 
 typedef long long llong;
 
