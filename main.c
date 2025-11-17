95018dcacc67fe04d74b0ca288490f5d2a3a6a37
lucas-mior <lucas.mior@tutamail.com>
2025-11-17 09:42:51 -0300

    cleanup

--- main.c
+++ main.c
@@ -26,7 +26,6 @@
 #define MAX_THREADS BRN2_MAX_THREADS
 #define COMPARE brn2_compare
 #include "sort.c"
-#include "assert.c"
 
 bool brn2_options_fatal = DEBUGGING;
 bool brn2_options_implicit = false;
