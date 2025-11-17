3603a30d8b5138c747badd20e896c892801f8d80
lucas-mior <lucas.mior@tutamail.com>
2025-11-17 18:46:36 -0300

    assert.c: print bit width of integers

--- generic.h
+++ generic.h
@@ -169,6 +169,30 @@ typename(Type type) {
     }
 }
 
+static uint32
+typebits(Type type) {
+    switch (type) {
+    case TYPE_LDOUBLE: return sizeof(ldouble)*CHAR_BIT;
+    case TYPE_DOUBLE:  return sizeof(double)*CHAR_BIT;
+    case TYPE_FLOAT:   return sizeof(float)*CHAR_BIT;
+    case TYPE_SCHAR:   return sizeof(schar)*CHAR_BIT;
+    case TYPE_SHORT:   return sizeof(short)*CHAR_BIT;
+    case TYPE_INT:     return sizeof(int)*CHAR_BIT;
+    case TYPE_LONG:    return sizeof(long)*CHAR_BIT;
+    case TYPE_LLONG:   return sizeof(llong)*CHAR_BIT;
+    case TYPE_UCHAR:   return sizeof(uchar)*CHAR_BIT;
+    case TYPE_USHORT:  return sizeof(ushort)*CHAR_BIT;
+    case TYPE_UINT:    return sizeof(uint)*CHAR_BIT;
+    case TYPE_ULONG:   return sizeof(ulong)*CHAR_BIT;
+    case TYPE_ULLONG:  return sizeof(ullong)*CHAR_BIT;
+    case TYPE_CHARP:   return sizeof(char *)*CHAR_BIT;
+    case TYPE_VOIDP:   return sizeof(void *)*CHAR_BIT;
+    case TYPE_BOOL:    return sizeof(bool)*CHAR_BIT;
+    case TYPE_CHAR:    return sizeof(char)*CHAR_BIT;
+    default:           return 0;
+    }
+}
+
 static ldouble
 ldouble_get(LongDoubleUnion var, Type type) {
     switch (type) {
