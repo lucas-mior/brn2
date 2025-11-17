9acc509f220723a6f6181618e92e8f530ee73360
lucas-mior <lucas.mior@tutamail.com>
2025-11-17 08:40:53 -0300

    avoid defining integers everywhere

--- util.c
+++ util.c
@@ -149,26 +149,6 @@ static void __attribute__((format(printf, 1, 2))) error(char *format, ...);
 #endif
 #endif
 
-#if !defined(INTEGERS)
-#define INTEGERS
-typedef unsigned char uchar;
-typedef unsigned short ushort;
-typedef unsigned int uint;
-typedef unsigned long ulong;
-typedef unsigned long long ullong;
-
-typedef long long llong;
-
-typedef int8_t int8;
-typedef int16_t int16;
-typedef int32_t int32;
-typedef int64_t int64;
-typedef uint8_t uint8;
-typedef uint16_t uint16;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
-#endif
-
 #if DEBUGGING || TESTING_util
 #if defined(__clang__)
 #pragma clang diagnostic ignored "-Wc11-extensions"
