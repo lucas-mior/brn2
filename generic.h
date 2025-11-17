464d40c9f961f14ea9bd5935b5f0abe766a90cd3
lucas-mior <lucas.mior@tutamail.com>
2025-11-17 19:00:09 -0300

    use upper case for unsupported dummy function

--- generic.h
+++ generic.h
@@ -28,7 +28,7 @@ typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
 
-void unsupported_type_for_generic(void);
+void UNSUPPORTED_TYPE_FOR_GENERIC(void);
 
 #define TYPENAME(VAR) \
 _Generic((VAR), \
