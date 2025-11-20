dd9d37dcba3ec98811b3db618f35a5c097235a04
lucas-mior <lucas.mior@tutamail.com>
2025-11-20 19:09:51 -0300

    add missing primitives

--- generic.c
+++ generic.c
@@ -335,12 +335,12 @@ main(void) {
     assert(MAXOF(primitive.aullong)  == ULLONG_MAX);
     assert(MAXOF(primitive.abool)    == 1);
 
-    assert(!strcmp(TYPENAME(primitive.aldouble),
-                   typename(TYPEID(primitive.aldouble))));
-    assert(!strcmp(TYPENAME(primitive.adouble),
-                   typename(TYPEID(primitive.adouble))));
-    assert(!strcmp(TYPENAME(primitive.afloat),
-                   typename(TYPEID(primitive.afloat))));
+    assert(!strcmp(TYPENAME(primitive.avoidp),
+                   typename(TYPEID(primitive.avoidp))));
+    assert(!strcmp(TYPENAME(primitive.acharp),
+                   typename(TYPEID(primitive.acharp))));
+    assert(!strcmp(TYPENAME(primitive.abool),
+                   typename(TYPEID(primitive.abool))));
     assert(!strcmp(TYPENAME(primitive.aschar),
                    typename(TYPEID(primitive.aschar))));
     assert(!strcmp(TYPENAME(primitive.ashort),
@@ -361,8 +361,12 @@ main(void) {
                    typename(TYPEID(primitive.aulong))));
     assert(!strcmp(TYPENAME(primitive.aullong),
                    typename(TYPEID(primitive.aullong))));
-    assert(!strcmp(TYPENAME(primitive.abool),
-                   typename(TYPEID(primitive.abool))));
+    assert(!strcmp(TYPENAME(primitive.afloat),
+                   typename(TYPEID(primitive.afloat))));
+    assert(!strcmp(TYPENAME(primitive.adouble),
+                   typename(TYPEID(primitive.adouble))));
+    assert(!strcmp(TYPENAME(primitive.aldouble),
+                   typename(TYPEID(primitive.aldouble))));
 
     {
         int32 var_int32;
