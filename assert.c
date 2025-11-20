f1a92ebe521c414169d58093f576a6675ff34e56
lucas-mior <lucas.mior@tutamail.com>
2025-11-20 13:35:18 -0300

    style

--- assert.c
+++ assert.c
@@ -260,18 +260,18 @@ _Generic((VAR2), \
 void UNSUPPORTED_TYPE_FOR_GENERIC_A_FIRST_SIGNED(void);
 
 #define A_BOTH_UNSIGNED(MODE, VAR1, VAR2, TYPE1, TYPE2) \
-  a_both_unsigned_##MODE(__FILE__, __LINE__, \
-                         #VAR1, #VAR2, \
-                         typename(TYPE1), typename(TYPE2), \
-                         TYPEBITS(TYPE1), TYPEBITS(TYPE2), \
-                         (ullong)(VAR1), (ullong)(VAR2))
-
-#define A_UNSIGNED_SIGNED(MODE, VAR1, VAR2, TYPE1, TYPE2) \
-  a_unsigned_signed_##MODE(__FILE__, __LINE__, \
-                           #VAR1, #VAR2, \
+    a_both_unsigned_##MODE(__FILE__, __LINE__,               \
+                           #VAR1, #VAR2,                     \
                            typename(TYPE1), typename(TYPE2), \
                            TYPEBITS(TYPE1), TYPEBITS(TYPE2), \
-                           (ullong)(VAR1), (llong)(VAR2))
+                           (ullong)(VAR1), (ullong)(VAR2))
+
+#define A_UNSIGNED_SIGNED(MODE, VAR1, VAR2, TYPE1, TYPE2) \
+    a_unsigned_signed_##MODE(__FILE__, __LINE__,               \
+                             #VAR1, #VAR2,                     \
+                             typename(TYPE1), typename(TYPE2), \
+                             TYPEBITS(TYPE1), TYPEBITS(TYPE2), \
+                             (ullong)(VAR1), (llong)(VAR2))
 
 #define A_FIRST_UNSIGNED(MODE, VAR1, VAR2, TYPE1) \
 _Generic((VAR2), \
