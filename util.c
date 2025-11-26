96361edfaa0e3bb8ec64cbb9eca4c327118fd312
lucas-mior <lucas.mior@tutamail.com>
2025-11-26 10:55:44 -0300

    only test while debugging

--- util.c
+++ util.c
@@ -330,10 +330,12 @@ strncmp64(char *left, char *right, int64 size) {
     if (size == 0) {
         return 0;
     }
-    if ((ullong)size >= (ullong)SIZE_MAX) {
-        error("Error in %s: Size (%lld) is bigger than SIZEMAX\n", __func__,
-              (llong)size);
-        fatal(EXIT_FAILURE);
+    if (DEBUGGING) {
+        if ((ullong)size >= (ullong)SIZE_MAX) {
+            error("Error in %s: Size (%lld) is bigger than SIZEMAX\n", __func__,
+                  (llong)size);
+            fatal(EXIT_FAILURE);
+        }
     }
     result = strncmp(left, right, (size_t)size);
     return result;
