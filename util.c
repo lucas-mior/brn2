94219a25c0cc14098c000ec9b23672c24357d788
lucas-mior <lucas.mior@tutamail.com>
2025-11-26 10:54:18 -0300

    only test while debugging

--- util.c
+++ util.c
@@ -235,14 +235,16 @@ INLINE void \
 CAT(func, 64)(void *dest, void *source, int64 size) { \
     if (size == 0) \
         return; \
-    if (size < 0) { \
-        error("Error in %s: Invalid size = %lld\n", __func__, (llong)size); \
-        fatal(EXIT_FAILURE); \
-    } \
-    if ((ullong)size >= (ullong)SIZE_MAX) { \
-        error("Error in %s: Size (%lld) is bigger than SIZEMAX\n", \
-               __func__, (llong)size); \
-        fatal(EXIT_FAILURE); \
+    if (DEBUGGING) { \
+        if (size < 0) { \
+            error("Error in %s: Invalid size = %lld\n", __func__, (llong)size); \
+            fatal(EXIT_FAILURE); \
+        } \
+        if ((ullong)size >= (ullong)SIZE_MAX) { \
+            error("Error in %s: Size (%lld) is bigger than SIZEMAX\n", \
+                   __func__, (llong)size); \
+            fatal(EXIT_FAILURE); \
+        } \
     } \
     func(dest, source, (size_t)size); \
     return; \
