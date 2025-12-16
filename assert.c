583fdb4ef1b78037e295af97fbd01e0268e07104
lucas-mior <lucas.mior@tutamail.com>
2025-12-16 15:19:11 -0300

    please every friend compiler

--- assert.c
+++ assert.c
@@ -40,16 +40,12 @@
 #endif
 
 #if !defined(trap)
-#if TESTING_assert
-#define trap(...) raise(SIGILL)
-#else
 #if defined(__GNUC__) || defined(__clang__)
 #define trap(...) __builtin_trap()
 #elif defined(_MSC_VER)
 #define trap(...) __debugbreak()
 #else
 #define trap(...) *(volatile int *)0 = 0
-#endif
 #endif
 #endif
 
