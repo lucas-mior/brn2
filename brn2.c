388578835f2e4c09279982e8178ad34b56e42247
lucas-mior <lucas.mior@tutamail.com>
2025-12-16 08:39:02 -0300

    copyright style

--- brn2.c
+++ brn2.c
@@ -4,9 +4,9 @@
  * Copyright (C) 2023-2025 Mior, Lucas;
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the*License,
+ * or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
