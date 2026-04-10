5a05d2efe71c3cbe7990f50630adcbbe63f02707
lucas-mior <lucas.mior@tutamail.com>
2026-04-10 20:38:34 -0300

    style

--- brn2.c
+++ brn2.c
@@ -743,8 +743,8 @@ brn2_sort(FileList *old) {
     clock_gettime(CLOCK_MONOTONIC_RAW, &t0);
 #endif
 
-    p = brn2_threads(brn2_threads_work_sort, old->length, old, NULL, NULL, 0,
-                     NULL);
+    p = brn2_threads(brn2_threads_work_sort,
+                     old->length, old, NULL, NULL, 0, NULL);
     if (p == 1) {
         free(dummy_last, dummy_size);
         return;
