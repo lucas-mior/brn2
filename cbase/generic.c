cca6fb1656cafa1431a5097d5399ffa110682069
lucas-mior <lucas.mior@tutamail.com>
2026-04-15 13:57:47 -0300

    style

--- cbase/generic.c
+++ cbase/generic.c
@@ -314,26 +314,26 @@ void UNSUPPORTED_TYPE_FOR_LDOUBLE_GET_GENERIC(void);
 
 #define LDOUBLE_GET(x) \
 _Generic((x), \
-    void*:   ldouble_from_voidp,  \
-    char*:   ldouble_from_charp,  \
-    bool:    ldouble_from_bool,   \
-    char:    ldouble_from_char,   \
-    schar:   ldouble_from_schar,  \
-    short:   ldouble_from_short,  \
-    int:     ldouble_from_int,    \
-    long:    ldouble_from_long,   \
-    llong:   ldouble_from_llong,  \
-    uchar:   ldouble_from_uchar,  \
-    ushort:  ldouble_from_ushort, \
-    uint:    ldouble_from_uint,   \
-    ulong:   ldouble_from_ulong,  \
-    ullong:  ldouble_from_ullong, \
-    float:   ldouble_from_float,  \
-    double:  ldouble_from_double, \
-    default: _Generic((x), \
-        ldouble: ldouble_from_ldouble, \
+    void*:   ldouble_from_voidp,                          \
+    char*:   ldouble_from_charp,                          \
+    bool:    ldouble_from_bool,                           \
+    char:    ldouble_from_char,                           \
+    schar:   ldouble_from_schar,                          \
+    short:   ldouble_from_short,                          \
+    int:     ldouble_from_int,                            \
+    long:    ldouble_from_long,                           \
+    llong:   ldouble_from_llong,                          \
+    uchar:   ldouble_from_uchar,                          \
+    ushort:  ldouble_from_ushort,                         \
+    uint:    ldouble_from_uint,                           \
+    ulong:   ldouble_from_ulong,                          \
+    ullong:  ldouble_from_ullong,                         \
+    float:   ldouble_from_float,                          \
+    double:  ldouble_from_double,                         \
+    default: _Generic((x),                                \
+        ldouble: ldouble_from_ldouble,                    \
         default: UNSUPPORTED_TYPE_FOR_LDOUBLE_GET_GENERIC \
-    ) \
+    )                                                     \
 )(x)
 
 #if defined(__GNUC__) || defined(__clang__)
