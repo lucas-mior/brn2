14c72e6638bc83ca779242b1dc698bf215bbcf4c
lucas-mior <lucas.mior@tutamail.com>
2026-07-22 15:28:58 -0300

    more test.sh

--- test.sh
+++ test.sh
@@ -17,7 +17,8 @@ for f in b c d a; do
 done
 
 cleanup () {
-    rm a b c d bxx
+    rm -f a b c d bxx
+    rm -f "/tmp/rename" "/tmp/rename2"
 }
 
 trap cleanup EXIT
@@ -52,8 +53,38 @@ done
 
 set -x
 bin/brn2 -f "/tmp/rename" -t "/tmp/rename2"
+set +x
 
 check a c
 check b bxx
+check c d
+check d a
+
+rm -f a b c d bxx
+rm -rf "/tmp/rename" "/tmp/rename2"
+
+for f in a b c d; do
+    echo "$f" > "$f"
+done
+
+# Keep four unique names after filtering, but make the unfiltered list large
+# enough that its precomputed indexes exceed the filtered hash set allocation.
+awk 'BEGIN {
+    for (i = 0; i < 16384; i += 1) {
+        printf "a\nb\nc\nd\n"
+    }
+    print "a"
+}' > "/tmp/rename"
+
+for f in b c d a; do
+    echo "$f" >> "/tmp/rename2"
+done
+
+set -x
+bin/brn2 -q -f "/tmp/rename" -t "/tmp/rename2" 2>/dev/null
+set +x
+
+check a b
+check b c
 check c d
 check d a
