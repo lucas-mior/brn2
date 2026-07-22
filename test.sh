f9d1c15666c230068a09ad2d3788d8a20f55089f
lucas-mior <lucas.mior@tutamail.com>
2026-07-22 15:17:10 -0300

    more test.sh

--- test.sh
+++ test.sh
@@ -17,7 +17,7 @@ for f in b c d a; do
 done
 
 cleanup () {
-    rm a b c d
+    rm a b c d bxx
 }
 
 trap cleanup EXIT
@@ -31,8 +31,29 @@ check () {
         exit 1
     fi
 }
+set +x
 
 check a b
 check b c
+check c d
+check d a
+
+rm -rf "/tmp/rename" "/tmp/rename2"
+
+for f in a b c d; do
+    echo "$f" >  "$f"
+    echo "$f" >> "/tmp/rename"
+done
+echo "a" >> "/tmp/rename"
+
+for f in c bxx d a; do
+    echo "$f" >> "/tmp/rename2"
+done
+
+set -x
+bin/brn2 -f "/tmp/rename" -t "/tmp/rename2"
+
+check a c
+check b bxx
 check c d
 check d a
