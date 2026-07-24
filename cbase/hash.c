e60c99be19c085a95a2fa35ad0cacf6e79c41c95
lucas-mior <lucas.mior@tutamail.com>
2026-07-23 23:46:14 -0300

    cleanup

--- cbase/hash.c
+++ cbase/hash.c
@@ -25,11 +25,6 @@
 #define HASH_H
 
 #include <math.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <time.h>
 
