c08686314365f122fb1b856830418150ebec9ffd
lucas-mior <lucas.mior@tutamail.com>
2026-07-26 15:55:05 -0300

    wip

--- cbase/base_macros.h
+++ cbase/base_macros.h
@@ -127,6 +127,7 @@ _Generic((SIZE), \
   #endif
 #endif
 
+// cproc uses gcc pre processor but __has_include does not work properly
 #if !defined(__CPROC__) && defined(__has_include)
   #if __has_include(<valgrind/valgrind.h>)
     #include <valgrind/valgrind.h>
