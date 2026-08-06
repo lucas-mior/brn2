08679b665cc5af0776f3dcc6f87c7bc7c508a2ef
lucas-mior <lucas.mior@tutamail.com>
2026-08-06 19:16:24 -0300

    wip

--- cbase/minmax.c
+++ cbase/minmax.c
@@ -268,6 +268,8 @@ _Generic((VAR1), \
   #define TYPEOF(...)
 #endif
 
+// Can't cast to the type directly because _Generic has to have all expressions
+// be compatible, even if they aren't selected
 #define MIN(VAR1, VAR2)                     \
   _Generic((VAR1),                          \
     int: _Generic((VAR2),                 \
