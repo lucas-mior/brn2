dc541e6bf04736747d0ab75aa1a1ad0349e9148c
lucas-mior <lucas.mior@tutamail.com>
2026-08-06 20:16:43 -0300

    wip

--- cbase/generic.c
+++ cbase/generic.c
@@ -15,45 +15,6 @@
 #include "base_macros.h"
 #include "libc.h"
 
-enum Type {
-    TYPE_VOIDP = 1,
-    TYPE_CHARP,
-    TYPE_BOOL,
-    TYPE_CHAR,
-    TYPE_SCHAR,
-    TYPE_SHORT,
-    TYPE_INT,
-    TYPE_LONG,
-    TYPE_LLONG,
-    TYPE_UCHAR,
-    TYPE_USHORT,
-    TYPE_UINT,
-    TYPE_ULONG,
-    TYPE_ULLONG,
-    TYPE_FLOAT,
-    TYPE_DOUBLE,
-    TYPE_OTHER = 0,
-};
-
-union Primitive {
-    void *avoidp;
-    char *acharp;
-    bool abool;
-    char achar;
-    schar aschar;
-    short ashort;
-    int aint;
-    long along;
-    llong allong;
-    uchar auchar;
-    ushort aushort;
-    uint auint;
-    ulong aulong;
-    ullong aullong;
-    float afloat;
-    double adouble;
-};
-
 static int strlen2(char *string) {
     return (int)strlen(string);
 }
