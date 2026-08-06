77036a8725955828be83b0cbb53f689062154ac8
lucas-mior <lucas.mior@tutamail.com>
2026-08-06 20:06:53 -0300

    cleanup

--- cbase/minmax.c
+++ cbase/minmax.c
@@ -260,70 +260,13 @@ _Generic((VAR1), \
 #undef MAX
 #endif
 
-// use typeof for the appropriate type, since we are mainly interested in
-// mainstream compiler warnings anyway
-#if CC_CLANG || CC_GCC || CC_TCC
-  #define CAST(X) (__typeof__(X))
-#else
-  #define CAST(...)
-#endif
-
 #define MIN_SAME(VAR1, VAR2) ((VAR1) < (VAR2) ? VAR1 : VAR2)
 #define MAX_SAME(VAR1, VAR2) ((VAR1) > (VAR2) ? VAR1 : VAR2)
 
-// IDEA: Cast to the the type if both operands are of the same type
-// or the second one has smaller type.
-// This silence complete irrelevant warnings automatically,
-// but keeps warnings about storing the result to a smaller variable
-// Can't cast to the type directly
-// because _Generic has to have all expressions be compatible,
-// even if they aren't selected
-#define MIN(VAR1, VAR2)                                   \
-  _Generic((VAR1),                                        \
-    schar: _Generic((VAR2),                               \
-        schar:     MIN_SAME(VAR1, VAR2),                  \
-        default:   MIN_IMPL(VAR1, VAR2)),                 \
-    short: _Generic((VAR2),                               \
-        schar:     MIN_SAME(VAR1, VAR2),                  \
-        short:     MIN_SAME(VAR1, VAR2),                  \
-        default:   MIN_IMPL(VAR1, VAR2)),                 \
-    int: _Generic((VAR2),                                 \
-        schar:     MIN_SAME(VAR1, VAR2),                  \
-        short:     MIN_SAME(VAR1, VAR2),                  \
-        int:       MIN_SAME(VAR1, VAR2),                  \
-        default:   MIN_IMPL(VAR1, VAR2)),                 \
-    long: _Generic((VAR2),                                \
-        llong:     MIN_IMPL(VAR1, VAR2),                  \
-        double:    MIN_IMPL(VAR1, VAR2),                  \
-        float:     MIN_IMPL(VAR1, VAR2),                  \
-        default:   MIN_SAME(VAR1, VAR2)),                 \
-    uchar: _Generic((VAR2),                               \
-        uchar:     MIN_SAME(VAR1, VAR2),                  \
-        default:   MIN_IMPL(VAR1, VAR2)),                 \
-    ushort: _Generic((VAR2),                              \
-        uchar:     MIN_SAME(VAR1, VAR2),                  \
-        ushort:    MIN_SAME(VAR1, VAR2),                  \
-        default:   MIN_IMPL(VAR1, VAR2)),                 \
-    uint: _Generic((VAR2),                                \
-        uchar:     MIN_SAME(VAR1, VAR2),                  \
-        ushort:    MIN_SAME(VAR1, VAR2),                  \
-        uint:      MIN_SAME(VAR1, VAR2),                  \
-        default:   MIN_IMPL(VAR1, VAR2)),                 \
-    default:       MIN_IMPL(VAR1, VAR2)                   \
-  )
-
-#define MAX(VAR1, VAR2)                                               \
-  _Generic((VAR1),                                                    \
-    int: _Generic((VAR2),                                             \
-        int: CAST(VAR1)MAX_IMPL(VAR1, VAR2),                          \
-        default: MAX_IMPL(VAR1, VAR2)),                               \
-    default: MAX_IMPL(VAR1, VAR2)                                     \
-  )
-
 // gives warnings because of returning always long
 // even when smaller integers are used
-/* #define MIN(A, B) MIN_IMPL(A, B) */
-/* #define MAX(A, B) MAX_IMPL(A, B) */
+#define MIN(A, B) MIN_IMPL(A, B)
+#define MAX(A, B) MAX_IMPL(A, B)
 
 #define MIN_IMPL(VAR1, VAR2) MINMAX_COMPARE(min, VAR1, VAR2)
 #define MAX_IMPL(VAR1, VAR2) MINMAX_COMPARE(max, VAR1, VAR2)
