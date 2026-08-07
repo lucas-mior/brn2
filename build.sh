2a14d4b8f385484f954dc0d70b98a43d191ea5b8
lucas-mior <lucas.mior@tutamail.com>
2026-08-07 17:36:59 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -2,22 +2,10 @@
 
 # shellcheck disable=SC2086,SC2031 
 
-set -e
-
-error () {
-    >&2 printf "$@"
-    return
-}
-
-if [ -n "$BASH_VERSION" ]; then
-    # shellcheck disable=SC3044
-    shopt -s expand_aliases
-fi
-
-alias trace_on='set -x'
-alias trace_off='{ set +x; } 2>/dev/null'
-
 dir=$(dirname "$(readlink -f "$0")")
+# shellcheck source=/dev/null
+. "$dir/cbase/common.sh"
+
 CPPFLAGS="$CPPFLAGS -I$dir/cbase"
 cd "$dir" || exit
 program=$(basename "$(readlink -f "$(dirname "$0")")")
@@ -141,7 +129,6 @@ with_toy_cc () {
     done
     return 0
 }
-
 
 case "$target" in
 debug)
