9b894c40d8ebdc96725faf7b9a1d7d81428b49bf
lucas-mior <lucas.mior@tutamail.com>
2026-08-07 16:46:31 -0300

    wip

--- build.sh
+++ build.sh
@@ -60,13 +60,15 @@ CFLAGS="$CFLAGS -Wno-char-subscripts"
 
 OS=$(uname -a)
 
-requested_cc=${CC:-}
 case "$target" in
-debug|test|fast_feedback)
-    CC="${requested_cc:-tcc}"
+debug|test)
+    CC="${CC:-tcc}"
+    ;;
+fast_feedback)
+    CC="${CC:-clang}"
     ;;
 *)
-    CC="${requested_cc:-cc}"
+    CC="${CC:-cc}"
     ;;
 esac
 
