93ae260cb0f9d2471dd5fe297bf8a5fc74e2ee59
lucas-mior <lucas.mior@tutamail.com>
2026-08-08 20:21:21 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -36,8 +36,8 @@ CPPFLAGS="$CPPFLAGS -D_DEFAULT_SOURCE"
 CFLAGS="$CFLAGS -std=c11"
 CFLAGS="$CFLAGS -Wfatal-errors"
 CFLAGS="$CFLAGS -Wextra -Wall -Wpedantic"
-# CFLAGS="$CFLAGS -Werror=all -Werror=extra"
-# CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
+CFLAGS="$CFLAGS -Werror=all -Werror=extra"
+CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 CFLAGS="$CFLAGS -Wno-cast-qual"
 CFLAGS="$CFLAGS -Wno-char-subscripts"
 CFLAGS="$CFLAGS -Wno-constant-logical-operand"
