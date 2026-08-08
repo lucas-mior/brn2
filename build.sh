19d5158cab483666133d0c450165217c11122271
lucas-mior <lucas.mior@tutamail.com>
2026-08-08 11:30:35 -0300

    wip

--- build.sh
+++ build.sh
@@ -50,7 +50,7 @@ CPPFLAGS="$CPPFLAGS -D_DEFAULT_SOURCE"
 
 CFLAGS="$CFLAGS -std=c11"
 CFLAGS="$CFLAGS -Wfatal-errors"
-CFLAGS="$CFLAGS -Wextra -Wall"
+CFLAGS="$CFLAGS -Wextra -Wall -Wpedantic"
 CFLAGS="$CFLAGS -Werror=all -Werror=extra"
 CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 CFLAGS="$CFLAGS -Wno-cast-qual"
