701c5fe508aae306e9934a80d5d3f9f1242aa6e7
lucas-mior <lucas.mior@tutamail.com>
2026-08-09 20:41:28 -0300

    disable -Werror

--- build.sh
+++ build.sh
@@ -38,7 +38,7 @@ CFLAGS="$CFLAGS -std=c11"
 CFLAGS="$CFLAGS -Wfatal-errors"
 CFLAGS="$CFLAGS -Wextra -Wall -Wpedantic"
 CFLAGS="$CFLAGS -Werror=all -Werror=extra"
-CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
+# CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 
 if [ "$CC" = "clang" ]; then
     CFLAGS="$CFLAGS -Weverything"
