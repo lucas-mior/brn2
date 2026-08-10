1493448f854d9e716f71d1a6d9b4280ca2a55c9b
lucas-mior <lucas.mior@tutamail.com>
2026-08-09 23:11:19 -0300

    _Generic: use functions

--- windows_functions.c
+++ windows_functions.c
@@ -108,7 +108,6 @@ scandir(
     int64 pattern_length;
     int64 count;
     int64 capacity = 16;
-    int32 result;
     int32 wide_dir_length;
     (void)filter;
     (void)compar;
@@ -215,15 +214,14 @@ scandir(
         return -1;
     }
 
-    if (count >= MAXOF(result)) {
+    if (count >= MAXOF(scandir)) {
         scandir_list_free(list, count, capacity);
         errno = EOVERFLOW;
         return -1;
     }
 
-    result = (int32)count;
     *namelist = list;
-    return result;
+    return (int32)count;
 }
 
 static time_t
