73e14a4b2257c381f39b04ee9a977f7e056a4a7a
lucas-mior <lucas.mior@tutamail.com>
2026-08-14 15:27:51 -0300

    reference: specialized memmem

--- brn2.c
+++ brn2.c
@@ -458,6 +458,44 @@ brn2_is_invalid_name(char *filename) {
     return true;
 }
 
+static inline char *
+memmem_slashslash(char *haystack, int64 haystack_len) {
+    int64 last;
+
+    if (haystack_len < 2) {
+        return NULL;
+    }
+
+    last = haystack_len - 1;
+    for (int64 i = 0; i < last; i += 1) {
+        if ((haystack[i] == '/') && (haystack[i + 1] == '/')) {
+            return haystack + i;
+        }
+    }
+
+    return NULL;
+}
+
+static inline char *
+memmem_slash_dot_slash(char *haystack, int64 haystack_len) {
+    int64 last;
+
+    if (haystack_len < 3) {
+        return NULL;
+    }
+
+    last = haystack_len - 2;
+    for (int64 i = 0; i < last; i += 1) {
+        if ((haystack[i] == '/')
+            && (haystack[i + 1] == '.')
+            && (haystack[i + 2] == '/')) {
+            return haystack + i;
+        }
+    }
+
+    return NULL;
+}
+
 void
 brn2_free_list(FileList *list) {
     if (DEBUGGING) {
@@ -502,9 +540,7 @@ brn2_threads_work_normalization(Work *arg) {
 
         // Note: leading // is not preserved, even though it can be used for
         // special purposes in some operating systems.
-        while ((p = memmem64(name + off,
-                             file->length - off,
-                             STRLIT("//")))) {
+        while ((p = memmem_slashslash(name + off, file->length - off))) {
             off = p - name;
 
             memmove64(&p[0], &p[1], file->length - off);
@@ -517,9 +553,7 @@ brn2_threads_work_normalization(Work *arg) {
         }
 
         off = 0;
-        while ((p = memmem64(name + off,
-                             file->length - off,
-                             STRLIT("/./")))) {
+        while ((p = memmem_slash_dot_slash(name + off, file->length - off))) {
             off = p - name;
 
             memmove64(&p[1], &p[3], file->length - off - 2);
