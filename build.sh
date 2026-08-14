7145d321af454ea82fc09002919678cb23319cda
lucas-mior <lucas.mior@tutamail.com>
2026-08-14 12:37:17 -0300

    organize callgrind

--- build.sh
+++ build.sh
@@ -330,12 +330,20 @@ callgrind|valgrind)
 
     trace_on
 
-    # cat "$original" \
-    # | valgrind --log-file="$dir/valgrind.1.txt" -s --tool=$tool \
-    #     "$dir/$exe" -q -f - --file-target "$shuffled"
-    valgrind --log-file="$dir/valgrind.2.txt" -s --tool=$tool \
+    cat "$original" \
+    | valgrind -s                                                     \
+       --log-file="$dir/valgrind.1.txt" --tool=$tool                  \
+       --callgrind-out-file=$dir/brn2_$1.out                          \
+        "$dir/$exe" -q -f - --file-target "$shuffled"
+
+    valgrind -s                                                       \
+        --log-file="$dir/valgrind.2.txt" --tool=$tool                 \
+       --callgrind-out-file=$dir/brn2_$1.out                          \
         "$dir/$exe" -q -d . --file-target "$rotated_left"
-    valgrind --log-file="$dir/valgrind.3.txt" -s --tool=$tool \
+
+    valgrind -s                                                       \
+       --log-file="$dir/valgrind.3.txt" --tool=$tool                  \
+       --callgrind-out-file=$dir/brn2_$1.out                          \
         "$dir/$exe" -q -f "$original" --file-target "$rotated_right"
 
     trace_off
