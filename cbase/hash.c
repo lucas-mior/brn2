68fd70a29e5a867bf9fc4374ecc28c82470eb169
lucas-mior <lucas.mior@tutamail.com>
2026-08-18 12:49:46 -0300

    prepare math functions

--- cbase/hash.c
+++ cbase/hash.c
@@ -785,14 +785,24 @@ hash_length(void *map) {
     return map2->length;
 }
 
+INLINE double
+hash_pow(double x) {
+    return x;
+}
+
+INLINE double
+hash_round(double x) {
+    return x;
+}
+
 #if DEBUGGING
 INLINE uint32
 hash_expected_collisions(void *map) {
     CommonMap *map2 = map;
     double n = map2->length;
     double m = map2->capacity;
-    double result = n - m*(1 - pow((m - 1) / m, n));
-    return (uint32)(round(result));
+    double result = n - m*(1 - hash_pow((m - 1) / m, n));
+    return (uint32)(hash_round(result));
 }
 #endif
 
