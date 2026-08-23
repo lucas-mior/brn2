c3cb517adee56727bad2b7689f2788076429df72
lucas-mior <lucas.mior@tutamail.com>
2026-08-23 16:47:12 -0300

    msvc ok

--- cbase/minmax.c
+++ cbase/minmax.c
@@ -239,7 +239,6 @@ _Generic((VAR1),                                                     \
 #undef MAX
 #endif
 
-#if CC_GCC || CC_CLANG
 #define MINMAX_COMPARE_SAME_TYPE(SYMBOL, VAR1, VAR2) \
     ((VAR1) SYMBOL (VAR2) ? (VAR1) : (VAR2))
 
@@ -247,6 +246,8 @@ _Generic((VAR1),                                                     \
     MINMAX_COMPARE_SAME_TYPE(<, VAR1, VAR2)
 #define MINMAX_COMPARE_SAME_TYPE_max(VAR1, VAR2) \
     MINMAX_COMPARE_SAME_TYPE(>, VAR1, VAR2)
+
+#if CC_GCC || CC_CLANG
 
 #define MINMAX_COMPARE_TYPE_PRESERVING(VAR) \
 _Generic((VAR),                             \
@@ -286,6 +287,32 @@ _Generic((VAR),                             \
 
 #define MIN(VAR1, VAR2) MINMAX_COMPARE_DIAGNOSTIC(min, VAR1, VAR2)
 #define MAX(VAR1, VAR2) MINMAX_COMPARE_DIAGNOSTIC(max, VAR1, VAR2)
+#elif CC_MSVC
+#define MINMAX_COMPARE_MSVC_TYPE(MODE, VAR1, VAR2, TYPE) \
+_Generic((VAR2),                                         \
+    TYPE: CAT(MINMAX_COMPARE_SAME_TYPE_, MODE)(VAR1, VAR2), \
+    default: MINMAX_COMPARE(MODE, VAR1, VAR2)             \
+)
+
+#define MINMAX_COMPARE_MSVC(MODE, VAR1, VAR2)             \
+_Generic((VAR1),                                         \
+    schar:  MINMAX_COMPARE_MSVC_TYPE(MODE, VAR1, VAR2, schar), \
+    short:  MINMAX_COMPARE_MSVC_TYPE(MODE, VAR1, VAR2, short), \
+    int:    MINMAX_COMPARE_MSVC_TYPE(MODE, VAR1, VAR2, int), \
+    long:   MINMAX_COMPARE_MSVC_TYPE(MODE, VAR1, VAR2, long), \
+    llong:  MINMAX_COMPARE_MSVC_TYPE(MODE, VAR1, VAR2, llong), \
+    uchar:  MINMAX_COMPARE_MSVC_TYPE(MODE, VAR1, VAR2, uchar), \
+    ushort: MINMAX_COMPARE_MSVC_TYPE(MODE, VAR1, VAR2, ushort), \
+    uint:   MINMAX_COMPARE_MSVC_TYPE(MODE, VAR1, VAR2, uint), \
+    ulong:  MINMAX_COMPARE_MSVC_TYPE(MODE, VAR1, VAR2, ulong), \
+    ullong: MINMAX_COMPARE_MSVC_TYPE(MODE, VAR1, VAR2, ullong), \
+    float:  MINMAX_COMPARE_MSVC_TYPE(MODE, VAR1, VAR2, float), \
+    double: MINMAX_COMPARE_MSVC_TYPE(MODE, VAR1, VAR2, double), \
+    default: MINMAX_COMPARE(MODE, VAR1, VAR2)              \
+)
+
+#define MIN(VAR1, VAR2) MINMAX_COMPARE_MSVC(min, VAR1, VAR2)
+#define MAX(VAR1, VAR2) MINMAX_COMPARE_MSVC(max, VAR1, VAR2)
 #else
 #define MIN(VAR1, VAR2) MINMAX_COMPARE(min, VAR1, VAR2)
 #define MAX(VAR1, VAR2) MINMAX_COMPARE(max, VAR1, VAR2)
