6413284a287d4648c5f2c1de170530b4e0d87b3f
lucas-mior <lucas.mior@tutamail.com>
2026-08-30 18:22:33 -0300

    not working

--- build.sh
+++ build.sh
@@ -70,8 +70,8 @@ CPPFLAGS="$CPPFLAGS -I. -Icbase"
 
 CFLAGS="$CFLAGS -std=c11"
 CFLAGS="$CFLAGS -Wfatal-errors"
-CFLAGS="$CFLAGS -Wextra -Wall -Wpedantic"
-CFLAGS="$CFLAGS -Werror=all -Werror=extra"
+# CFLAGS="$CFLAGS -Wextra -Wall -Wpedantic"
+# CFLAGS="$CFLAGS -Werror=all -Werror=extra"
 # CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 
 if [ "$CC" = "clang" ] || [ "$CC" = "zig cc" ]; then
---
+++ cbase/warnings.h
@@ -0,0 +1,6 @@
+#if !defined(WARNINGS_H)
+#define WARNINGS_H
+
+#pragma GCC diagnostic warning "-Wunused-variable"
+
+#endif
