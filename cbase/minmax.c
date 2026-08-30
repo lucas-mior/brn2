4b5222ec3d3498fdef09612895a6f72ab333b346
lucas-mior <lucas.mior@tutamail.com>
2026-08-30 18:15:53 -0300

    wip

--- cbase/minmax.c
+++ cbase/minmax.c
@@ -10,10 +10,10 @@
 #define TESTING_minmax 0
 #endif
 
+#include "platform_detection.h"
 #include "libc.h"
 #include "primitives.h"
 #include "base_macros.h"
-#include "platform_detection.h"
 
 #define GENERATE_COMPARE_POINTERS(MODE, SYMBOL)                                \
 static void *                                                                  \
