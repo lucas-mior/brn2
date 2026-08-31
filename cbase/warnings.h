347de4ff001b4f5c06def60e96e3ec4211798348
lucas-mior <lucas.mior@tutamail.com>
2026-08-30 21:04:48 -0300

    wip

--- cbase/warnings.h
+++ cbase/warnings.h
@@ -27,6 +27,7 @@
   #pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
   #pragma clang diagnostic ignored "-Wunused-macros"
   #pragma clang diagnostic ignored "-Wused-but-marked-unused"
+  #pragma clang diagnostic ignored "-Wpadded"
 #endif
 
 #if DEBUGGING || TESTING
