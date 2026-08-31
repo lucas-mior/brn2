f6b9b35276c1de269a48dacd9343f1502d52413a
lucas-mior <lucas.mior@tutamail.com>
2026-08-31 11:25:52 -0300

    cleanup

--- cbase/util.c
+++ cbase/util.c
@@ -269,14 +269,7 @@ memchr64(void *pointer, int32 value, int64 size) {
     if (size == 0) {
         return 0;
     }
-#if CC_CLANG
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdisabled-macro-expansion"
-#endif
     return memchr(pointer, value, (size_t)size);
-#if CC_CLANG
-#pragma clang diagnostic pop
-#endif
 }
 
 int
