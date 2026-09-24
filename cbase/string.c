e04e404c5a487eed0f2430d2ba14a205510199f3
lucas-mior <lucas.mior@tutamail.com>
2026-09-24 12:23:28 -0300

    style (whitespace)

--- cbase/string.c
+++ cbase/string.c
@@ -502,8 +502,7 @@ sb_printf(StrBuilder *str, char *fmt, ...) {
 
     sb_reserve(str, estimate);
 
-    len = fmt_vsnprintf(str->data + str->len,
-                        (int64)estimate + 1, fmt, ap2);
+    len = fmt_vsnprintf(str->data + str->len, (int64)estimate + 1, fmt, ap2);
     va_end(ap2);
     if (len < 0) {
         error("Error formatting \"%s\".", fmt);
