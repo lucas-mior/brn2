344e8da8751a0fcf095a64c734c7953c381de9aa
lucas-mior <lucas.mior@tutamail.com>
2026-09-26 15:22:02 -0300

    style (whitespace)

--- cbase/assertions.c
+++ cbase/assertions.c
@@ -747,20 +747,20 @@ GENERATE_ASSERT_DOUBLE_SIGN(non_negative, >=, ">= 0")
 
 #undef GENERATE_ASSERT_DOUBLE_SIGN
 
-#define GENERATE_ASSERT_LDOUBLE_SIGN(MODE, SYMBOL, EXPECTED)                   \
-void                                                                           \
-a_sign_ldouble_##MODE(char *file, int32 line, char *func,                      \
-                      char *name, ldouble var) {                               \
-    if (!(var SYMBOL (ldouble)0)) {                                            \
-        if (DEBUGGING) {                                                       \
-            assert_error(file, line, func, "%s = %Lf " EXPECTED "\n",          \
-                         name, var);                                           \
-            TRAP();                                                            \
-        } else {                                                               \
-            UNREACHABLE();                                                     \
-        }                                                                      \
-    }                                                                          \
-    return;                                                                    \
+#define GENERATE_ASSERT_LDOUBLE_SIGN(MODE, SYMBOL, EXPECTED)            \
+void                                                                    \
+a_sign_ldouble_##MODE(char *file, int32 line, char *func,               \
+                      char *name, ldouble var) {                        \
+    if (!(var SYMBOL (ldouble)0)) {                                     \
+        if (DEBUGGING) {                                                \
+            assert_error(file, line, func, "%s = %Lf " EXPECTED "\n",   \
+                         name, var);                                    \
+            TRAP();                                                     \
+        } else {                                                        \
+            UNREACHABLE();                                              \
+        }                                                               \
+    }                                                                   \
+    return;                                                             \
 }
 
 GENERATE_ASSERT_LDOUBLE_SIGN(positive, >, "> 0")
